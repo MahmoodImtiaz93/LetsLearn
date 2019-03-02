b046768d110fc2372f71a0294e1efb8b9a39746e
mahmoodimtiaz93 <be.nintythree@gmail.com>
2019-03-02 20:37:06 +0600

    Test

--- app/src/main/java/com/mahmoodimtiaz/letslearn/MainActivity.java
+++ app/src/main/java/com/mahmoodimtiaz/letslearn/MainActivity.java
@@ -8,13 +8,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mbutton = findViewById(R.id.button4);
+
+
 
     }
 
